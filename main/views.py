b8f841d6dba6388d06e8f8e144c82c77382d1db9
Ricardo E. Alvarado Mata <ricardorxz82@gmail.com>
2019-12-03 16:38:28 -0700

    dashboard agregado

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from   django.views.generic.list import ListView
 
-# Create your views here.
+def home(request):
+    return render(request, 'main/default.html')
