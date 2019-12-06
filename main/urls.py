15f9f9df517a4c0fa5b6dba925a2a2ad6096be6a
Brayan Durazo <brayandurazo@Brayans-MacBook-Pro.local>
2019-12-06 16:28:27 -0700

    Ver info curso

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from . import VerCurso
 from .home_view import HomeView
 
 app_name = 'main'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('Curso/<int:curso_id>/', VerCurso.detail, name='detail'),
 ]
